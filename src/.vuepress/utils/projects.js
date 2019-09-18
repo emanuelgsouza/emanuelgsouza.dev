71a1aa30ea9f7e9e93b8a8dcff00d7de8881c103
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-18 15:37:39 -0300

    implement projects list

---
+++ src/.vuepress/utils/projects.js
@@ -0,0 +1,60 @@
+export default {
+  'pt-br': [
+    {
+      name: 'Acessibilidade for Devs',
+      description: 'Um site para quem quer aprender sobre acessibilidade. Voltado para o mundo dos devs, você encontra lá inúmeras dicas de como tornar seu site acessível. Aborda-se desde boas práticas a referências a documentação oficial da W3C. O site está em constante crescimento, e como um projeto Open Source, está sempre aberto a melhorias e críticas.',
+      url: 'https://acessibilidade-for-devs.github.io/',
+      repository: 'https://github.com/acessibilidade-for-devs/acessibilidade-for-devs.github.io',
+      'tags': ['Acessibilidade', 'Front-End'],
+      image: '/projects-acessibility.jpg'
+    },
+    {
+      name: 'Como meu deputado foi eleito?',
+      description: "Você conhece o conceito de puxadinho? Este projeto visa trazer de maneira rápida a informação de quais Deputados foram 'puxados' e quais foram eleitos pelos seus próprios votos. O objetivo é o usuário poder buscar seus deputados ou ter um apanhado geral de como foram as eleições no Brasil, em 2018",
+      url: 'https://legislativo-br.github.io/',
+      repository: 'https://github.com/legislativo-br',
+      tags: ["React", "Front-End", "Python", "Nodejs"],
+      image: '/projects-legislative.jpg'
+    },
+    {
+      name: 'Help-Me!',
+      description: 'Tive a oportunidade de colocar em prática meus conhecimentos usando o Firebase, bem como o Framework Quasar, neste projeto. É um sistema para cadastro de problemas que potencialmente serão apreciados pela Coordenação de TI da Universidade Unigranrio, sendo possível virar TCC ou PCA (Projeto Curricular Articulador)',
+      url: 'https://help-me.surge.sh',
+      repository: 'https://github.com/emanuelgsouza/help-me',
+      tags: ["Vuejs", "Quasar", "Front-end", "Firebase"],
+      image: '/projects-helpme.jpg'
+    },
+    {
+      name: 'Análise de Dados das Eleições em 2016',
+      description: 'Foi a oportunidade de desenvolver um projeto de Análise de Exploratória de Dados nas eleições para Prefeito de Duque de Caxias. Foram algumas descobertas bacanas. Foi o primeiro projeto usando conhecimentos de estatística, bem como da análise exploratória dos dados. Uma oportunidade para se trabalhar com a linguagem Python.',
+      url: null,
+      repository: 'https://github.com/emanuelgsouza/eleicoes-dc-2016',
+      tags: ["Análise de Dados Exploratória", "Python"],
+      image: '/projects-nopreview.png'
+    },
+    {
+      name: 'ERSI - 2019',
+      description: 'Uma oportunidade de trabalhar em grupo para o desenvolvimento do site para a Escola Regional de Sistemas de Informação, um evento da Sociedade Brasileira de Computação, edição 2019, que será sediado na Universidade Unigranrio, polo Caxias - RJ',
+      url: 'http://www.unigranrio.com.br/ersi2019/',
+      repository: null,
+      tags: ["Site Institucional"],
+      image: '/projects-ersi2019.jpg'
+    },
+    {
+      name: 'Emilena Gonçalves - Página profissional',
+      description: 'Minha primeira oportunidade de usar React e Next, e usei a biblioteca Talwind também. Desenvolvi a parte de design e código. Mais uma oportunidade de trabalhar com ambos num mesmo projeto',
+      url: 'https://emilenagoncalves.com.br/',
+      repository: null,
+      tags: ["Página profissional", "Next", "React", "Tailwind"],
+      image: '/projects-emilenagoncalves.jpg'
+    },
+    {
+      name: 'Star Wars - Planets Quiz',
+      description: 'Mais um projeto para exercitar o uso do Vue.js, dessa vez com o Tailwind e testes com a bibliotece Jest. O quiz consiste em questionar ao usuário em qual planeta um determinado personagem do Universo Star Wars nasceu. Os dados vem da API de Star Wars',
+      url: 'https://vue-star-wars-quiz.emanuelgsouza.dev/',
+      repository: 'https://github.com/emanuelgsouza/vue-star-wars-quiz',
+      tags: ["Vue.js", "Jest", "Tailwind"],
+      image: '/projects-star-wars-quiz.jpg'
+    }
+  ]
+}
---
+++ src/projects.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <CNav />
+
+    <div class="projects-page has-fixed-nav">
+      <div class="container">
+        <h1 class="title"> Conheça alguns dos meus projetos </h1>
+
+        <!-- PROJECTS -->
+        <div class="projects-container">
+          <div
+            v-for="(project, key) in projects"
+            :key="key"
+            class="column is-4"
+          >
+            <CCardProject :project="project" />
+          </div>
+        </div>
+      </div>
+    </div>    
+  </main>
+</template>
+
+<script>
+import projectData from './.vuepress/utils/projects'
+
+export default {
+  name: 'ProjectsPage',
+  data: () => ({
+    projects: projectData['pt-br']
+  })
+}
+</script>
+
+<style scoped>
+.projects-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+}
+</style>

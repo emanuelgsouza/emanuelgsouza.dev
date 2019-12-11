1329984d9bcc9a9dc97cfd9225dc4cc1478940ed
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-12-10 23:41:48 -0300

    setup PWA and Google Analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,38 @@
 const tailwindcss = require("tailwindcss")
 
+const siteConfig = {
+  title: 'Emanuel Gonçalves - Front End Enginner',
+  color: '#2f3439'
+}
+
 module.exports = {
-  siteName: 'Emanuel Gonçalves - Front End Enginner',
-  plugins: [],
+  siteName: siteConfig.title,
+  plugins: [
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+          title: siteConfig.title,
+          startUrl: '/',
+          display: 'standalone',
+          statusBarStyle: 'default',
+          manifestPath: 'manifest.json',
+          serviceWorkerPath: 'service-worker.js',
+          cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+          shortName: siteConfig.title,
+          themeColor: siteConfig.color,
+          backgroundColor: '#ffffff',
+          icon: './src/favicon.png',
+          msTileImage: './src/favicon.png',
+          msTileColor: siteConfig.color
+      }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: process.env.GA
+      }
+    }
+  ],
   css: {
     loaderOptions: {
       postcss: {

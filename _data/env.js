ee8ba7956ea53fef78fef3eaed55c9b00d218fa6
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-07-26 22:58:48 -0300

    chore: use `HOST_NAME` environment variable in production URL

--- _data/env.js
+++ _data/env.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // inpiring in https://jec.fyi/blog/setting-up-seo-and-google-analytics
 const environment = process.env.ELEVENTY_ENV;
 const PROD_ENV = 'prod';
-const prodUrl = 'https://emanuelgsouza.dev';
+const prodUrl = process.env.HOST_NAME;
 const devUrl = 'http://localhost:8080';
 const baseUrl = environment === PROD_ENV ? prodUrl : devUrl;
 const isProd = environment === PROD_ENV;

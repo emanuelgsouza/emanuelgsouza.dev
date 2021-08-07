02d9e81646079d963647bba522ef1cc250aa1787
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-08-07 08:50:12 -0300

    fix: use storyblok version in post collection data

--- _data/posts.js
+++ _data/posts.js
@@ -2,4 +2,5 @@ const storyblokClient = require('../config/storyblok');
 
 module.exports = async () => storyblokClient.getAll('cdn/stories', {
   starts_with: 'posts',
+  version: process.env.STORYBLOK_VERSION,
 });

eb9942a136e9cea25edda5a0e3cce5b589f5b5b1
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-08-12 19:54:24 -0300

    refactor: refactor posts collection data from Storyblok rewriting the data property to content

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,31 @@
 const storyblokClient = require('../../config/storyblok');
 
-module.exports = async () => storyblokClient.getAll('cdn/stories', {
-  starts_with: 'posts',
-  version: process.env.STORYBLOK_VERSION,
-});
+const changeDataToContent = (post) => {
+  const content = { ...post.content };
+  const clone = { ...post };
+
+  delete clone.content;
+
+  return {
+    ...clone,
+    data: content,
+  };
+};
+
+const transformPosts = (posts) => posts.map(changeDataToContent);
+
+module.exports = async () => {
+  try {
+    const posts = await storyblokClient.getAll('cdn/stories', {
+      starts_with: 'posts',
+      version: process.env.STORYBLOK_VERSION,
+      sort_by: 'first_published_at:desc',
+    });
+
+    return Promise.resolve(transformPosts(posts));
+  } catch (error) {
+    // eslint-disable-next-line
+    console.error('Error on getting the posts stories on Storyblok');
+    return Promise.reject(error);
+  }
+};

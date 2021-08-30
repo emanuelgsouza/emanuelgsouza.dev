b3cb030baaade641f54c50985d4f80470b450dcd
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-08-30 14:48:11 -0300

    chore: personalize link mark in RichtextResolver

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,34 @@ resolver.addNode('code_block', (node) => {
   }
 })
 
+resolver.addMark('link', (node) => {
+  const attrs = { ...node.attrs }
+  const { linktype = 'url' } = node.attrs
+
+  const isLinkTypeEmail = linktype === 'email'
+  if (isLinkTypeEmail) {
+    attrs.href = `mailto:${attrs.href}`
+  }
+
+  const hasAnchor = attrs.anchor
+  if (hasAnchor) {
+    attrs.href = `${attrs.href}#${attrs.anchor}`
+    delete attrs.anchor
+  }
+
+  const isNotStoryOpenNewTab = linktype !== 'story' && attrs.target === '_blank'
+  if (isNotStoryOpenNewTab) {
+    attrs.rel = 'noreferrer noopener'
+  }
+
+  return {
+    tag: [{
+      tag: 'a',
+      attrs: attrs
+    }]
+  }
+})
+
 module.exports = function (eleventyConfig) {
   // copy files
   eleventyConfig.addPassthroughCopy("src/images");

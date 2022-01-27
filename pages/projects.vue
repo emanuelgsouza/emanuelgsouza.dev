598071c470f41cc274ef0a0cd47fda7f7a0608be
Emanuel <emanuelgdesouza@gmail.com>
2022-01-27 20:19:39 -0300

    feat: implement project page

---
+++ pages/projects.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="section">
+    <AppTitle level="1">Meus projetos</AppTitle>
+
+    <div class="columns is-multiline">
+      <div v-for="project in projects" :key="project.id" class="column is-half">
+        <AppProjectCard :project="project.content" :tags="project.tag_list" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+const getProjects = (context) => {
+  const version =
+    context.query._storyblok || context.isDev ? 'draft' : 'published'
+
+  return context.app.$storyapi
+    .getAll('cdn/stories', {
+      version,
+      by_slugs: 'projects/*',
+    })
+    .then((res) => res)
+}
+
+export default {
+  async asyncData(context) {
+    try {
+      const projects = await getProjects(context)
+
+      return {
+        projects,
+      }
+    } catch (res) {
+      if (!res.response) {
+        console.error(res)
+        context.error({
+          statusCode: 404,
+          message: 'Failed to receive content form api',
+        })
+      } else {
+        console.error(res.response.data)
+        context.error({
+          statusCode: res.response.status,
+          message: res.response.data,
+        })
+      }
+    }
+  },
+
+  data: () => ({
+    projects: [],
+  }),
+
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new window.StoryblokBridge()
+
+      // Use the input event for instant update of content
+      storyblokInstance.on('input', (event) => {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content
+        }
+      })
+
+      // Use the bridge to listen the events
+      storyblokInstance.on(['published', 'change'], (event) => {
+        // window.location.reload()
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        })
+      })
+    })
+  },
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -6,11 +6,13 @@ import SbHome from '~/components/SbHome.vue'
 
 // own components
 import AppTags from '~/components/AppTags.vue'
+import AppTitle from '~/components/AppTitle.vue'
 import AppProjectCard from '~/components/AppProjectCard.vue'
 import AppPostCard from '~/components/AppPostCard.vue'
 
 Vue.component('home', SbHome)
 
 Vue.component('AppTags', AppTags)
+Vue.component('AppTitle', AppTitle)
 Vue.component('AppProjectCard', AppProjectCard)
 Vue.component('AppPostCard', AppPostCard)

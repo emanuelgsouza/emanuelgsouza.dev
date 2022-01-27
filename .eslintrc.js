cb9c39db9ccbbce9fb130592fb2743f34d3babbe
Emanuel <emanuelgdesouza@gmail.com>
2022-01-27 20:20:11 -0300

    chore: update no-console rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 'off',
+    'no-console': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
   },
 }
